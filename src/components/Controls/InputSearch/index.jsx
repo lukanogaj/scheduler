a5ba338fa6310e756e2792ff50e8e3f808fa42cd
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-11-22 11:38:07 +0000

    input-finally

--- src/components/Controls/InputSearch/index.jsx
+++ src/components/Controls/InputSearch/index.jsx
@@ -1,18 +1,23 @@
 import styles from "./index.module.scss";
-import searchIcon from "../../images/search.png";
+import searchIcon from "../../images/search-trans.png";
+import { IoSearchOutline } from "react-icons/io5";
 
 const SearchControl = () => {
+	const iconPosition = {
+		color: "white",
+		position: "absolute",
+		top: "24%",
+		right: "2%",
+		fontSize: "3em",
+	};
 	return (
 		<div className={styles.search}>
-			<img
-				src={searchIcon}
-				alt='search'
-			/>
 			<input
 				type='search'
 				placeholder='Search'
 				name='search'
 			/>
+			<IoSearchOutline style={iconPosition} />
 		</div>
 	);
 };
