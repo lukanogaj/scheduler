6751d8e6cc8835daefdf53b2394bad6db44b4dd3
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-12-04 09:11:57 +0000

    tasks-html-css

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -1,5 +1,20 @@
+import styles from "./index.module.scss";
+import { IoListSharp } from "react-icons/io5";
 const Tasks = () => {
-	return <div></div>;
+	const icon = {
+		color: "#DBDDE1",
+		fontSize: "3em",
+	};
+	return (
+		<div className={styles.tasks}>
+			<div className={styles.tasksHead}>
+				<IoListSharp style={icon} />
+				<h1>Tasks</h1>
+			</div>
+
+			<div className={styles.tasksCount}>4</div>
+		</div>
+	);
 };
 
 export default Tasks;
--- src/components/Workspace/index.jsx
+++ src/components/Workspace/index.jsx
@@ -1,10 +1,12 @@
 import styles from "./index.module.scss";
 import AllTasksNotes from "../AllTasksNotes";
+import Tasks from "../Tasks";
 
 const Workspace = () => {
 	return (
 		<div className={styles.workSpace}>
 			<AllTasksNotes />
+			<Tasks />
 		</div>
 	);
 };
