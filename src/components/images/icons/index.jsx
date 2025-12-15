c41929efb5d440e0ffc38eb31b37a2ae08ae66f1
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-12-15 08:44:50 +0000

    images

--- src/components/images/icons/index.jsx
+++ src/components/images/icons/index.jsx
@@ -125,34 +125,34 @@ export const Menu = (props) => (
 	</svg>
 );
 
-// export const NotesIcon = (props) => (
-// 	<svg
-// 		xmlns='http://www.w3.org/2000/svg'
-// 		{...props}
-// 		width='24'
-// 		height='24'
-// 		viewBox='0 0 24 24'
-// 		fill='none'
-// 		stroke='currentColor'
-// 		stroke-width={2}
-// 		stroke-linecap='round'
-// 		stroke-linejoin='round'
-// 		class='lucide lucide-notepad-text-icon lucide-notepad-text'>
-// 		<path d='M8 2v4' />
-// 		<path d='M12 2v4' />
-// 		<path d='M16 2v4' />
-// 		<rect
-// 			width='16'
-// 			height='18'
-// 			x='4'
-// 			y='4'
-// 			rx='2'
-// 		/>
-// 		<path d='M8 10h6' />
-// 		<path d='M8 14h8' />
-// 		<path d='M8 18h5' />
-// 	</svg>
-// );
+export const NotesIcon = (props) => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		{...props}
+		width='24'
+		height='24'
+		viewBox='0 0 24 24'
+		fill='none'
+		stroke='currentColor'
+		stroke-width={2}
+		stroke-linecap='round'
+		stroke-linejoin='round'
+		class='lucide lucide-notepad-text-icon lucide-notepad-text'>
+		<path d='M8 2v4' />
+		<path d='M12 2v4' />
+		<path d='M16 2v4' />
+		<rect
+			width='16'
+			height='18'
+			x='4'
+			y='4'
+			rx='2'
+		/>
+		<path d='M8 10h6' />
+		<path d='M8 14h8' />
+		<path d='M8 18h5' />
+	</svg>
+);
 export const SplitScreen = (props) => (
 	<svg
 		viewBox='0 0 24 24'
