fe4609b0eb28c68fe1e98d8b2893ceb06a43996b
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2026-02-14 10:15:08 +0000

    refactor(Content): introduce rightRail layout for header utilities

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,17 +1,4 @@
-import styles from "./Content.module.scss";
-// // import Sidebar from "../../components/Sidebar";
-
-// // const Content = ({ children, onAddTaskClick, headerLeft }) => {
-// // 	return (
-// // 		<div className={styles.content}>
-// // 			<Sidebar onAddTaskClick={onAddTaskClick} />
-
-// // 			<div className={styles.mainWrapper}>
-// // 				<main className={styles.main}>{children}</main>
-// // 			</div>
-// // 		</div>
-// // 	);
-// // };
+// import styles from "./Content.module.scss";
 
 // // export default Content;
 // import Sidebar from "../../components/Sidebar";
@@ -24,60 +11,26 @@ import styles from "./Content.module.scss";
 // 			<Sidebar onAddTaskClick={onAddTaskClick} />
 
 // 			<div className={styles.mainWrapper}>
-// 				{/* Full-width header bar */}
-// 				<header className={styles.header}>
-// 					{/* Clamped header content (title + date/time) */}
-// 					<div className={styles.headerInner}>
-// 						<div className={styles.headerLeft}>{headerLeft}</div>
-// 						<DateTimeClock />
-// 					</div>
-
-// 					{/* Pinned far-right utilities (icons only) */}
-// 					<div className={styles.headerUtilities}>
-// 						<HeaderUtilities />
-// 					</div>
-// 				</header>
-
-// 				{/* Clamped page content column */}
 // 				<div className={styles.inner}>
+// 					<header className={styles.header}>
+// 						<div className={styles.headerLeft}>{headerLeft}</div>
+
+// 						<div className={styles.headerRight}>
+// 							<DateTimeClock />
+// 						</div>
+// 					</header>
+
 // 					<main className={styles.main}>{children}</main>
 // 				</div>
+// 				<HeaderUtilities />
 // 			</div>
 // 		</div>
 // 	);
 // };
 
 // export default Content;
-// import Sidebar from "../../components/Sidebar";
-// import DateTimeClock from "../DateTimeClock/DateTimeClock";
-// import HeaderUtilities from "../HeaderUtilities/HeaderUtilities";
 
-// const Content = ({ children, onAddTaskClick, headerLeft }) => {
-// 	return (
-// 		<div className={styles.content}>
-// 			<Sidebar onAddTaskClick={onAddTaskClick} />
-
-// 			<div className={styles.mainWrapper}>
-// 				<header className={styles.header}>
-// 					<div className={styles.headerInner}>
-// 						<div className={styles.headerLeft}>{headerLeft}</div>
-// 						<DateTimeClock />
-// 					</div>
-
-// 					<div className={styles.headerUtilities}>
-// 						<HeaderUtilities />
-// 					</div>
-// 				</header>
-
-// 				<div className={styles.inner}>
-// 					<main className={styles.main}>{children}</main>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default Content;
+import styles from "./Content.module.scss";
 import Sidebar from "../../components/Sidebar";
 import DateTimeClock from "../DateTimeClock/DateTimeClock";
 import HeaderUtilities from "../HeaderUtilities/HeaderUtilities";
@@ -94,11 +47,15 @@ const Content = ({ children, onAddTaskClick, headerLeft }) => {
 
 						<div className={styles.headerRight}>
 							<DateTimeClock />
-							<HeaderUtilities />
 						</div>
 					</header>
 
 					<main className={styles.main}>{children}</main>
+				</div>
+
+				{/* ✅ pinned far-right icons */}
+				<div className={styles.rightRail}>
+					<HeaderUtilities />
 				</div>
 			</div>
 		</div>
